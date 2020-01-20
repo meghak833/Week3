53bee7923c58d7803b8c5b18ec91128b27f6f0eb
megha <megha.patel2@sheridancollege.ca>
2020-01-20 14:13:32 -0500

    remove "side" attribute from the class

--- src/beads/Beads.java
+++ src/beads/Beads.java
@@ -13,20 +13,18 @@ package beads;
 public class Beads {
     public String color;
     public char letter;
-    public int side;
-
+    
     Beads()
     {
             color="red";
             letter='R';
-            side=6;
     }
     
-    public Beads(String clr,char ltr, int s)
+    public Beads(String clr,char ltr)
     {
         color=clr;
         letter=ltr;    
-        side=s;
+        
     }
     /** getter for color
      * @return the color
