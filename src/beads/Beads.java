dc1424c415fb11121ddd95a989a489f60bfbdfbb
Hanna411 <hanzh@sheridancollege.ca>
2020-01-20 14:14:25 -0500

    change the author

--- src/beads/Beads.java
+++ src/beads/Beads.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package beads;
 
 /**
  *
- * @author Megha
+ * @author Zhaoxu Han
  */
 public class Beads {
     public String color;
