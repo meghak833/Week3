1af10e8ad5037d0edcd4f9d530f76f6e808cb0fc
EmesheNZ <emeshenz@gmail.com>
2020-01-20 14:30:41 -0500

    Removed licence header in test class

--- src/beads/BeadTest.java
+++ src/beads/BeadTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package beads;
 
@@ -28,4 +23,5 @@ public class BeadTest {
     }
     
 }
+
 
